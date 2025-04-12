b5959fcd1ee898397b6fcf45700b53f4367993d0
dotcom <emmanueltokyo24@gmail.com>
2025-04-12 16:18:15 +0100

    error fixed

--- Route.js
+++ Route.js
@@ -4,9 +4,7 @@ import News from "./Models/NewsPost.js";
 const router = express.Router();
 dotenv.config();
 
-router.get("/keep-alive", (req, res) => {
-  res.send(lastPingMessage);
-});
+
 router.get("/all-post",  (req, res) => News.getAllPost(req, res));
 
 export default router;
