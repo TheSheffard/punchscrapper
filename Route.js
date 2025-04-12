6dcd54c4d3f6ead3990b3a0f20656c9d1bb95d30
dotcom <emmanueltokyo24@gmail.com>
2025-04-12 16:28:56 +0100

    added new route

--- Route.js
+++ Route.js
@@ -1,10 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import News from "./Models/NewsPost.js";
+import LaunchPuppeteer from './Utils/Helper.js';
+
 const router = express.Router();
 dotenv.config();
 
 
-router.get("/all-post",  (req, res) => News.getAllPost(req, res));
+router.get("/all-post", (req, res) => {
+ 
+  News.getAllPost(req, res);
+});
+
+router.get("/pinged", (req, res) => {
+  const lastPingMessage = LaunchPuppeteer.getLastPingMessage();
+  res.json({ lastPingMessage });
+});
+
+
 
 export default router;
